51e97e9378cc69743c8ee1e2b28600cec47fe45c
MusicalNinjaDad <102677655+MusicalNinjaDad@users.noreply.github.com>
2026-08-24 08:02:04 +0200

    Devcontainer 793f0e83422b268826b84772f3010891065542c517af08f0a2ee54ffbab5416b (#13)

--- .devcontainer/graphify.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-set -euo pipefail
-
-sudo mkdir /workspaces/graphify
-sudo chown musicalninja:musicalninja /workspaces/graphify
-git clone --branch=feat/vibe-install https://github.com/xavierpestel-ai/graphify /workspaces/graphify
-uv tool install /workspaces/graphify
