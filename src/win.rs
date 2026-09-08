444d0ba88763d6085b4b4c40b15f18ca8967b1ff
Mike Foster <i.am.mike.foster@gmail.com>
2026-09-08 07:25:40 +0000

    lock CD

--- src/win.rs
+++ src/win.rs
@@ -511,6 +511,8 @@ impl AudioCdExtMut for AudioCd {
 
     #[expect(refining_impl_trait)]
     fn lock(self) -> ReadOnlyAudioCd {
+        tracing::trace!(audiocd = ?self, "locking");
+        
         ReadOnlyAudioCd {
             drive: self.drive,
             disc: self.disc,
