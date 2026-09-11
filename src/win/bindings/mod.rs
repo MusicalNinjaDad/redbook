76e617ad00043a808daa1504a89ac8d2e66baae4
Mike Foster <i.am.mike.foster@gmail.com>
2026-09-11 05:57:43 +0000

    and tidy the expects

--- src/win/bindings/mod.rs
+++ src/win/bindings/mod.rs
@@ -1,10 +1,11 @@
-#[expect(unsafe_code, reason = "generated bindings to windows API via ffi")]
+#![expect(unsafe_code, reason = "bindings to windows API via ffi")]
+
 #[expect(
     clippy::undocumented_unsafe_blocks,
+    nonstandard_style,
+    clippy::upper_case_acronyms,
     reason = "generated bindings to windows API via ffi"
 )]
-#[expect(nonstandard_style)]
-#[expect(clippy::upper_case_acronyms)]
 #[expect(
     dead_code,
     reason = "until we split and have an extra use for auto-generated stuff that we don't use"
