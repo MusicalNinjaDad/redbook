5aed9270ba7e477b31ed03fd89b82707cbddb3d4
Mike Foster <i.am.mike.foster@gmail.com>
2026-09-17 14:31:37 +0000

    trial app with test fixture

--- src/bin/gui/main.rs
+++ src/bin/gui/main.rs
@@ -6,9 +6,31 @@ mod album;
 mod slint;
 use slint::*;
 
+use redbook::{
+    Disc,
+    test_fixtures::albums::TestAlbum::{self, *},
+};
+
 #[cfg(feature = "gui")]
 fn main() {
-    MainWindow::new().unwrap().run().unwrap()
+    let album: TestAlbum = DefinitelyMaybe;
+
+    let toc = album.expected_toc();
+    let tracks = album.expected_tracks_minimal();
+    let leadout = album.expected_leadout();
+    let musicbrainz = album.expected_musicbrainz();
+
+    let mut disc = Disc::new(toc, tracks, leadout).unwrap();
+    disc.set_musicbrainz(musicbrainz);
+    for (release_id, image) in album.expected_thumbnails() {
+        disc.add_thumbnail(release_id, image);
+    }
+
+    let albums = AlbumDetails::for_disc(&disc).unwrap();
+
+    let app = MainWindow::new().unwrap();
+    app.set_albums(albums);
+    app.run().unwrap()
 }
 
 #[cfg(not(feature = "gui"))]
