5323e4c6af28fd5fc7065d7c1913e9e4bcf0e324
Mike Foster <i.am.mike.foster@gmail.com>
2026-09-19 08:21:05 +0000

    gui reads CD

--- src/bin/gui/main.rs
+++ src/bin/gui/main.rs
@@ -1,36 +1,32 @@
 #![cfg_attr(unstable_try_blocks_heterogeneous, feature(try_blocks_heterogeneous))]
-
 #[cfg(feature = "gui")]
 mod album;
 
 mod slint;
+
+use std::io;
+
+use redbook::{AudioCd, AudioCdExt, AudioCdExtMut, win::drive::all_drives};
+
 use slint::*;
 
-use redbook::{
-    Disc,
-    test_fixtures::albums::TestAlbum::{self, *},
-};
-
 #[cfg(feature = "gui")]
-fn main() {
-    let album: TestAlbum = DefinitelyMaybe;
+fn main() -> io::Result<()> {
+    let drive = all_drives()?
+        .next()
+        .ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "no CD found"))?;
+    let mut cd = AudioCd::try_from(drive)?;
 
-    let toc = album.expected_toc();
-    let tracks = album.expected_tracks_minimal();
-    let leadout = album.expected_leadout();
-    let musicbrainz = album.expected_musicbrainz();
+    cd.disc_mut().update_musicbrainz()?;
+    cd.disc_mut().update_thumbnails()?;
 
-    let mut disc = Disc::new(toc, tracks, leadout).unwrap();
-    disc.set_musicbrainz(musicbrainz);
-    for (release_id, image) in album.expected_thumbnails() {
-        disc.add_thumbnail(release_id, image);
-    }
-
-    let albums = AlbumDetails::for_disc(&disc).unwrap();
+    let disc = cd.disc();
+    let albums = AlbumDetails::for_disc(disc).unwrap();
 
     let app = MainWindow::new().unwrap();
     app.set_albums(albums);
-    app.run().unwrap()
+    app.run().unwrap();
+    Ok(())
 }
 
 #[cfg(not(feature = "gui"))]
