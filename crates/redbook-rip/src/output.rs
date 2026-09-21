db56a0e4c2879d9f6d0aac8a1e98606dc9d77ea7
Mike Foster <i.am.mike.foster@gmail.com>
2026-09-21 14:53:29 +0000

    rm bad lint

--- crates/redbook-rip/src/output.rs
+++ crates/redbook-rip/src/output.rs
@@ -1,7 +1,6 @@
 use std::io;
 use tracing_subscriber::{filter::LevelFilter, fmt::layer, prelude::*, registry};
 
-#[cfg_attr(not(target_family = "windows"), expect(dead_code))]
 pub fn init_tracing() -> io::Result<()> {
     let stdout_level = LevelFilter::DEBUG;
     let stdout = layer().with_filter(stdout_level);
