89740170175160bb559dde36f9cdcc182790d6ab
Mike Foster <i.am.mike.foster@gmail.com>
2026-09-22 13:19:34 +0000

    create folder to store encoded flacs (in gui) Fixes #77

--- crates/redbook-rip/src/main.rs
+++ crates/redbook-rip/src/main.rs
@@ -122,6 +122,8 @@ fn main() -> io::Result<()> {
 
                 let output_dir =
                     PathBuf::from(artist.sanitize_filename()).join(disc_title.sanitize_filename());
+                tracing::debug!(output_dir = %output_dir.display());
+                fs::create_dir_all(&output_dir)?;
                 // TODO: #24 handle invlaid chars in filenames: see https://learn.microsoft.com/en-us/windows/win32/fileio/naming-a-file#naming-conventions
                 let flac_path = output_dir.join(tag.filename()).with_extension("flac");
                 let mut flac_file = File::create_new(&flac_path).or_warn("creating flac file")?;
