6bd16b6bfefd41e1b9226b1b9fc185f51b51590c
MusicalNinjaDad <102677655+MusicalNinjaDad@users.noreply.github.com>
2026-09-26 17:30:09 +0200

    ensure thread cancellation if closed before ripping starts (#91)

--- crates/redbook-rip/src/main.rs
+++ crates/redbook-rip/src/main.rs
@@ -9,7 +9,10 @@ mod output;
 mod slint;
 
 use ::slint::{Model, ModelRc, VecModel, Weak};
-use crossbeam::select;
+use crossbeam::{
+    channel::{Sender, unbounded},
+    select,
+};
 
 use metaflac::{
     Block, Tag,
@@ -29,11 +32,9 @@ use std::{
     fs::{self, File},
     io::{self, Write},
     rc::Rc,
-    sync::{
-        Arc, Mutex,
-        mpsc::{self, Sender},
-    },
+    sync::{Arc, Mutex},
     thread,
+    time::Duration,
 };
 
 use tracing_result::Trace;
@@ -47,31 +48,35 @@ struct EncodingProgress {
 fn main() -> io::Result<()> {
     output::init_tracing()?;
     let app = MainWindow::new().unwrap();
+    let (controller, context) = Controller::<!>::new();
     let (rip_controller, rip_context) = Controller::<RipProgress>::new();
     let (enc_controller, enc_context) = Controller::<EncodingProgress>::new();
-    let enc_context2 = enc_context.clone();
 
     let drive = all_drives()?
         .next()
         .ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "no CD found"))?;
     let mut cd = AudioCd::try_from(drive)?;
-    cd.add_context(rip_context);
+    cd.add_context(rip_context.clone());
     let cd = Arc::new(Mutex::from(cd));
 
-    let (to_rip_tx, to_rip_rx) = mpsc::channel::<Vec<usize>>();
-    let (ripped_tx, ripped_rx) = mpsc::channel::<RippedTrack>();
+    let (to_rip_tx, to_rip_rx) = unbounded::<Vec<usize>>();
+    let (ripped_tx, ripped_rx) = unbounded::<RippedTrack>();
 
     let app_ = app.as_weak();
     let cd_ = cd.clone();
+    let context_ = context.clone();
     let setup = thread::spawn(move || {
         let cd = cd_;
-        {
+        let context = context_;
+        try bikeshed io::Result<()> {
             let mut disc_lock = cd.lock().expect("TODO #68 tracing on poison");
             let disc = disc_lock.disc_mut();
+            context.cancelled()?;
             disc.update_musicbrainz()?;
-            disc.update_thumbnails()?;
-        }
+            disc.update_thumbnails(context.clone())?;
+        }?;
 
+        context.cancelled()?;
         ::slint::invoke_from_event_loop(move || {
             let app = app_.clone().unwrap();
             let releases = {
@@ -88,21 +93,16 @@ fn main() -> io::Result<()> {
         .map_err(io::Error::other)
     });
 
-    let ripper = thread::spawn(move || try {
-        while let Ok(tracks) = to_rip_rx.recv() {
-            let cd_lock = cd
-                .lock()
-                .expect("TODO #68 error handling & tracing on poison");
-            let disc = cd_lock.disc();
-            for track_number in tracks {
-                let track = disc.track(track_number);
-                tracing::info!(ripping = ?track);
-                let ripped = cd_lock.rip(track_number).or_error("ripping")?;
-                ripped_tx
-                    .send(ripped)
-                    .map_err(io::Error::other)
-                    .or_error("sending")?;
-            }
+    let ripper = thread::spawn(move || try bikeshed io::Result<()> {
+        loop {
+            select! {
+                recv(to_rip_rx) -> tracks => {
+                    rip_tracks(cd.clone(), tracks.map_err(io::Error::other)?, ripped_tx.clone())?;
+                }
+                default(Duration::from_millis(100)) => {
+                    rip_context.cancelled()?;
+                }
+            };
         }
     });
 
@@ -111,6 +111,7 @@ fn main() -> io::Result<()> {
             doing: None,
             done: Vec::new(),
         };
+        // cancelling ripper drops ripped_tx, so we don't need to loop on a select here ...
         while let Ok(ripped) = ripped_rx.recv() {
             #[expect(unused_must_use, reason = "loop on error")]
             #[expect(
@@ -189,16 +190,19 @@ fn main() -> io::Result<()> {
     let enc_rx = enc_controller.receiver();
     let progress_updates = thread::spawn(move || try {
         loop {
-            enc_context2.cancelled()?;
             select! {
                 recv(rip_rx) -> progress => update_rip_progress(app_.clone(), progress.unwrap()),
                 recv(enc_rx) -> progress => update_encoding_progress(app_.clone(), progress.unwrap()),
+                default(Duration::from_millis(100)) => {
+                    context.cancelled()?;
+                }
             };
         }
     });
 
     app.run().unwrap();
     drop(app);
+    controller.cancel();
     rip_controller.cancel();
     enc_controller.cancel();
     tracing::debug!("app dropped, expecting threads to close now ...");
@@ -255,6 +259,27 @@ fn rip(app: Weak<MainWindow>, channel: Sender<Vec<usize>>) -> impl FnMut() {
             .send(tracks)
             .expect("TODO #70 error handling on broken channel");
     }
+}
+
+fn rip_tracks(
+    cd: Arc<Mutex<AudioCd>>,
+    tracks: Vec<usize>,
+    ripped_tracks: Sender<RippedTrack>,
+) -> io::Result<()> {
+    let cd_lock = cd
+        .lock()
+        .expect("TODO #68 error handling & tracing on poison");
+    let disc = cd_lock.disc();
+    for track_number in tracks {
+        let track = disc.track(track_number);
+        tracing::info!(ripping = ?track);
+        let ripped = cd_lock.rip(track_number).or_error("ripping")?;
+        ripped_tracks
+            .send(ripped)
+            .map_err(io::Error::other)
+            .or_error("sending")?;
+    }
+    Ok(())
 }
 
 fn update_rip_progress(app: Weak<MainWindow>, progress: RipProgress) {
